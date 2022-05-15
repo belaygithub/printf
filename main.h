cd1e47425502d982d2236744691626c96434df4d
belaygit <belayeshetu2014@gmail.com>
2022-05-15 06:47:29 -0700

    main.c file containing the prototype of the functions

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef PRINTF
+#define PRINTF
+int _printf(const char *format, ...);
+int _putchar(char c);
+int fun_string(va_list arguments);
+int fun_character(va_list arguments);
+int fun_integer(va_list arguments);
+#endif
