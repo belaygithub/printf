b13d11bfb10eb734c13a8d6d5ec87b30d5a99a9a
ABK4747 <abenaabena1894@gmai.com>
2022-05-17 03:30:47 -0700

    msg

--- _printf.c
+++ _printf.c
@@ -11,22 +11,6 @@
 static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
 
 	for (i = 0; p[i].t != NULL; i++)
 	{
@@ -80,4 +64,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(valist);
 	return (count);
-}
+u}
