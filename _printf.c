c8ab4c3f1677541dfd93cda54cd3f6849ac5f55e
ABK4747 <abenaabena1894@gmai.com>
2022-05-17 02:53:42 -0700

    msg

--- _printf.c
+++ _printf.c
@@ -1,79 +1,83 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
- * printIdentifiers - prints special characters
- * @next: character after the %
- * @arg: argument for the indentifier
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
- */
-int printIdentifiers(char next, va_list arg) 
-{
-int functsIndex;
-identifierStruct functs[] = {
-{"c", print_char},
-{"s", print_str},
-{"d", print_int},
-{"i", print_int},
-{"u", print_unsigned},
-{"S", print_STR},
-{NULL, NULL}
-};
-for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
-{
-if (functs[functsIndex].indentifier[0] == next)
-return (functs[functsIndex].printer(arg));
-}
-return (0); 
-}
-/**
- * _printf - mimic printf from stdio
- * Description: produces output according to a format
- * write output to stdout, the standard output stream
- * @format: character string composed of zero or more directive
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
  *
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
- * return -1 for incomplete identifier error
+ * Return: pointer to valid function or NULL
+ */
+
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-unsigned int i;
-int identifierPrinted = 0, charPrinted = 0;
-va_list arg;
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-va_start(arg, format);
-if (format == NULL)
-return (-1);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-_putchar(format[i]);
-charPrinted++;
-continue;
-}
-if (format[i + 1] == '%')
-{
-_putchar('%');
-charPrinted++;
-i++;
-continue;
-}
-if (format[i + 1] == '\0')
-return (-1);
-identifierPrinted = printIdentifiers(format[i + 1], arg);
-if (identifierPrinted == -1 || identifierPrinted != 0)
-i++;
-if (identifierPrinted > 0)
-charPrinted += identifierPrinted;
-if (identifierPrinted == 0)
-{
-_putchar('%');
-charPrinted++;
-}
-}
-va_end(arg);
-return (charPrinted);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,33 +1,33 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
 int _putchar(char c);
-int print_int(va_list arg);
-int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_str(va_list arg);
-int print_percent(void);
-void print_binary(unsigned int n, unsigned int *printed);
-int print_unsignedToBinary(va_list arg);*
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR(va_list arg);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-/**
- * struct identifierStruct - structure definition of a printTypeStruct
- * @indentifier: type
- * @printer: function to print
- */
-typedef struct identifierStruct
-{
-	char *indentifier;
-	int (*printer)(va_list);
-} identifierStruct;
-
-#endif
+#endif  /* _MAIN_H */
