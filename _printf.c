3b24a59e55d66b4cf764590747a87424bb70eb1f
belaygit <belayeshetu2014@gmail.com>
2022-05-17 12:52:57 -0700

    updates

--- _printf.c
+++ _printf.c
@@ -16,15 +16,6 @@ print_t p[] = {
 		       {"s", print_s},
 		       {"i", print_i},
 		       {"d", print_d},
-		       {"u", print_u},
-		       {"b", print_b},
-		       {"o", print_o},
-		       {"x", print_x},
-		       {"X", print_X},
-		       {"p", print_p},
-		       {"S", print_S},
-		       {"r", print_r},
-		       {"R", print_R},
 		       {NULL, NULL}
 	};
 	for (i = 0; p[i].t != NULL; i++)
