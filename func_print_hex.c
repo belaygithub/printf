79ac2438867d27e0afe97bf541f04b63d3d7959a
belaygit <belayeshetu2014@gmail.com>
2022-05-17 11:49:17 -0700

    updates

--- func_print_hex.c
+++ func_print_hex.c
@@ -61,26 +61,6 @@ int print_X(va_list X)
 {
 	return (print_hex(va_arg(X, unsigned int), 1));
 }
-
-/**
- * _pow - calculates an exponent
- * @base: base of exponent
- * @exponent: exponent of number
- *
- * Return: base ^ exponent
- */
-static unsigned long _pow(unsigned int base, unsigned int exponent)
-{
-	unsigned int i;
-	unsigned long ans = base;
-
-	for (i = 1; i < exponent; i++)
-	{
-		ans *= base;
-	}
-	return (ans);
-}
-
 /**
  * print_p - prints an address
  * @p: address to print
